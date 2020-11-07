4e998971c8478402293a6dc48f4510e3e03ccdea
woojiin <zzangwooojiin@gmail.com>
2020-11-07 14:31:57 -0500

    create a landing page of the website

--- fitness-tracker-app/src/index.js
+++ fitness-tracker-app/src/index.js
@@ -4,11 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//import * as serviceWorker from "./serviceWorker";
+import { Link, Route, Switch, BrowserRouter } from "react-router-dom";
+
+
+//landing page of the app
+const LandingPage = (
+    <div class="landingPage_main">
+        <h1 class="header"> Welcome to Fitness Tracker App </h1>
+        <h3 class="quote"> Success usually comes to those who are too busy to be looking for it... </h3>
+        <Link class="landingPage_enter" to="./App">
+            Enter Site
+        </Link>
+    </div>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/App" render={() => <App/>} />
+                <Route path="/" render={() => LandingPage} />
+            </Switch>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
